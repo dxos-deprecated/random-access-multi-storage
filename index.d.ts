9722e189c74cfed505a499f462a85a947f26e9ed
Dmytro Maretskyi <maretskii@gmail.com>
2020-10-27 14:52:14 +0100

    Extend types

--- index.d.ts
+++ index.d.ts
@@ -27,4 +27,16 @@ export interface File {
   read(offset: number, size: number, cb?: (err: Error | null, data?: Buffer) => void): void;
 
   write(offset: number, data: Buffer, cb?: (err: Error | null) => void): void;
+
+  del(offset: number, data: Buffer, cb?: (err: Error | null) => void): void;
+
+  stat(cb: (err: Error | null, data?: FileStat) => void): void;
+
+  close(cb?: (err: Error | null) => void): void;
+
+  destroy(cb?: (err: Error | null) => void): void
+}
+
+export interface FileStat {
+  size: number
 }
